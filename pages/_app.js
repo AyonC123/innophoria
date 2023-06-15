5524e100880870dd3f50480f4ebc69bf605fab15
AyonC <ayon@github.com>
2023-06-15 20:37:48 +0530

    edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
 import { SessionProvider } from "next-auth/react";
-import Head from "next/head"
+import Head from "next/head";
 
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({
-	Component,
-	pageProps: { session, ...pageProps },
+  Component,
+  pageProps: { session, ...pageProps },
 }) {
-	return (
-		<>
-			<SessionProvider session={session}>
-				<Head>
-					<title>Innophoria</title>
-				</Head>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</SessionProvider>
-		</>
-	);
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Head>
+          <title>Innophoria</title>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
+  );
 }
